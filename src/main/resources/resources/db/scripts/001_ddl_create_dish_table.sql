2f1ab7543ffd66aa857cc0f41910e4fa513ff3dc
SevaStopAll <Sevastopallrh@yandex.ru>
2023-05-12 15:12:23 +0400

    4. Docker сервисы [#504781]

--- src/main/resources/resources/db/scripts/001_ddl_create_dish_table.sql
+++
@@ -1,5 +0,0 @@
-create table IF NOT EXISTS status(
-    id serial primary key not null,
-    name varchar(2000) not null,
-    price integer not null
-);
--- src/main/resources/resources/db/scripts/002_dml_insert_dish_table.sql
+++
@@ -1,2 +0,0 @@
-insert into dish(name, description, price)
-values('test', 'test', 100);
